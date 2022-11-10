d38256ef811a1d9992921269384766e2972c7e0d
antklim <antklim@gmail.com>
2022-11-11 07:05:24 +1100

    feat: added sub method

--- main.ts
+++ main.ts
@@ -2,11 +2,16 @@ export function add(a: number, b: number): number {
   return a + b;
 }
 
+export function sub(a: number, b: number): number {
+  return a - b;
+}
+
 export function mul(a: number, b: number): number {
   return a * b;
 }
 
 if (import.meta.main) {
   console.log("Add 2 + 3 =", add(2, 3));
+  console.log("Sub 2 + 3 =", sub(2, 3));
   console.log("Mul 2 * 3 =", mul(2, 3));
 }
