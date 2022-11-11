58a37ffc9a67f4c3ee5031336d3ad226d0f326cf
antklim <antklim@gmail.com>
2022-11-12 08:18:08 +1100

    feat: added sin

--- main.ts
+++ main.ts
@@ -22,6 +22,10 @@ export function sqrt(a: number): number {
   return Math.sqrt(a);
 }
 
+export function sin(a: number): number {
+  return Math.sin(a);
+}
+
 if (import.meta.main) {
   console.log("Add 2 + 3 =", add(2, 3));
   console.log("Sub 2 - 3 =", sub(2, 3));
@@ -29,4 +33,5 @@ if (import.meta.main) {
   console.log("Div 2 / 3 =", div(2, 3));
   console.log("Sqr 2     =", sqr(2));
   console.log("Sqrt2     =", sqrt(2));
+  console.log("Sin 2     =", sin(2));
 }
